71784a91aaf57c1aa483ad298911c478e6e3e5ba
Rafael Bachmann <rafael.bachmann.93@gmail.com>
2023-11-04 20:16:07 +0100

    Initial commit

---
+++ minimal-dual-core-usb/src/main.rs
@@ -0,0 +1,183 @@
+#![cfg_attr(not(test), no_std)]
+#![no_main]
+#![feature(type_alias_impl_trait)]
+
+use defmt::info;
+use embassy_executor::Executor;
+use embassy_rp::{
+    bind_interrupts,
+    config::Config,
+    gpio::{Input, Level, Output, Pull},
+    i2c::{self, I2c},
+    multicore::{spawn_core1, Stack},
+    peripherals::{I2C0, PIN_25, PIN_4, PIN_5, USB},
+    usb::{self, InterruptHandler},
+    watchdog::Watchdog,
+};
+use embassy_sync::{blocking_mutex::raw::CriticalSectionRawMutex, channel::Channel};
+use embassy_time::Timer;
+use embassy_usb::{class::cdc_acm::CdcAcmClass, UsbDevice};
+use embedded_graphics::{
+    mono_font::{ascii::FONT_10X20, MonoTextStyle},
+    pixelcolor::BinaryColor,
+    prelude::{DrawTarget, Point},
+    text::{Alignment, Text},
+    Drawable,
+};
+use formatter::Formatter;
+use minimal_usb_protocol::{HostToSensor, SensorToHost, CONFIG_VERSION};
+use ssd1306::{
+    mode::BufferedGraphicsMode,
+    prelude::{DisplayConfig, I2CInterface},
+    rotation::DisplayRotation,
+    size::DisplaySize128x64,
+    Ssd1306,
+};
+use static_cell::StaticCell;
+
+use crate::usb_device::initialize;
+use {defmt_rtt as _, panic_probe as _};
+
+use crate::transport::transport;
+
+use core::fmt::Write;
+
+mod formatter;
+mod transport;
+mod usb_device;
+
+// Global constants.
+pub const ID: [u8; 24] = *b"Minimal USB device\0\0\0\0\0\0";
+
+pub const STYLE: MonoTextStyle<'_, BinaryColor> = MonoTextStyle::new(&FONT_10X20, BinaryColor::On);
+
+// Global channels.
+pub static OUTGOING: Channel<CriticalSectionRawMutex, SensorToHost, 128> =
+    Channel::<CriticalSectionRawMutex, SensorToHost, 128>::new();
+
+pub static INCOMING: Channel<CriticalSectionRawMutex, HostToSensor, 128> =
+    Channel::<CriticalSectionRawMutex, HostToSensor, 128>::new();
+
+// Executors.
+static EXECUTOR0: StaticCell<Executor> = StaticCell::new();
+static mut CORE1_STACK: Stack<65_536> = Stack::new();
+static EXECUTOR1: StaticCell<Executor> = StaticCell::new();
+
+// Bind interrupts to the handlers inside embassy.
+bind_interrupts!(struct Irqs {
+    I2C0_IRQ => i2c::InterruptHandler<I2C0>;
+    USBCTRL_IRQ => InterruptHandler<USB>;
+});
+
+#[cortex_m_rt::entry]
+fn main() -> ! {
+    info!("Hello there!");
+
+    let peripherals = embassy_rp::init(Config::default());
+
+    let mut i2c0_config = i2c::Config::default();
+    i2c0_config.frequency = 400_000;
+
+    let i2c0_sda = peripherals.PIN_0;
+    let i2c0_scl = peripherals.PIN_1;
+
+    let oled_reset = Output::new(peripherals.PIN_4, Level::Low);
+
+    // Start pin @ DSub #10.
+    let start = Input::new(peripherals.PIN_5, Pull::None);
+
+    let led = Output::new(peripherals.PIN_25, Level::Low);
+
+    let i2c0_bus = i2c::I2c::new_async(peripherals.I2C0, i2c0_scl, i2c0_sda, Irqs, i2c0_config);
+    let display_interface = ssd1306::I2CDisplayInterface::new(i2c0_bus);
+    let display = Ssd1306::new(
+        display_interface,
+        DisplaySize128x64,
+        DisplayRotation::Rotate0,
+    )
+    .into_buffered_graphics_mode();
+
+    let watchdog = Watchdog::new(peripherals.WATCHDOG);
+
+    let (usb_device, usb_class) = initialize(peripherals.USB, Irqs);
+
+    spawn_core1(peripherals.CORE1, unsafe { &mut CORE1_STACK }, move || {
+        let executor1 = EXECUTOR1.init(Executor::new());
+        executor1.run(|spawner| {
+            spawner
+                .spawn(do_things(oled_reset, display, start, led, watchdog))
+                .unwrap();
+        })
+    });
+
+    let executor0 = EXECUTOR0.init(Executor::new());
+    executor0.run(|spawner| {
+        spawner.spawn(run_usb_device(usb_device)).unwrap();
+        spawner.spawn(run_protocol(usb_class)).unwrap();
+    });
+}
+
+#[embassy_executor::task]
+pub async fn do_things(
+    mut oled_reset: Output<'static, PIN_4>,
+    mut display: Ssd1306<
+        I2CInterface<I2c<'static, I2C0, i2c::Async>>,
+        DisplaySize128x64,
+        BufferedGraphicsMode<DisplaySize128x64>,
+    >,
+    mut start: Input<'static, PIN_5>,
+    mut led: Output<'static, PIN_25>,
+    watchdog: Watchdog,
+) -> ! {
+    oled_reset.set_high();
+    oled_reset.set_low();
+    Timer::after_millis(10).await;
+    oled_reset.set_high();
+
+    display.init().expect("Display connected?");
+    display.clear(BinaryColor::Off).unwrap();
+    display.flush().unwrap();
+
+    let mut buf: Formatter<96> = Formatter::new();
+
+    let mut counter = 0;
+    loop {
+        start.wait_for_high().await;
+        led.set_high();
+        display.clear(BinaryColor::Off).unwrap();
+        display.flush().unwrap();
+        info!("doing things {}", counter);
+        write!(buf, "Counter: {counter}").unwrap();
+        OUTGOING
+            .send(SensorToHost::Id {
+                name: ID,
+                version: CONFIG_VERSION,
+            })
+            .await;
+        Text::with_alignment(buf.as_str(), Point::new(64, 15), STYLE, Alignment::Center)
+            .draw(&mut display)
+            .unwrap();
+        display.flush().unwrap();
+        counter += 1;
+        buf.clear();
+        start.wait_for_low().await;
+        led.set_low();
+    }
+}
+
+#[embassy_executor::task]
+pub async fn run_usb_device(
+    usb_device: &'static mut UsbDevice<'static, usb::Driver<'static, USB>>,
+) {
+    usb_device.run().await;
+}
+
+#[embassy_executor::task]
+pub async fn run_protocol(class: &'static mut CdcAcmClass<'static, usb::Driver<'static, USB>>) {
+    loop {
+        class.wait_connection().await;
+        info!("Connected");
+        let _ = transport(class).await;
+        info!("Disconnected");
+    }
+}
---
+++ minimal-usb-server/src/main.rs
@@ -0,0 +1,135 @@
+use std::str::from_utf8;
+
+use anyhow::Context;
+use arguments::{Arguments, Command};
+use bytes::Bytes;
+use clap::Parser;
+use futures::{stream::StreamExt, SinkExt};
+use minimal_usb_protocol::*;
+use tokio::{
+    select,
+    sync::mpsc::{self, error::SendError},
+    task::spawn_blocking,
+    try_join,
+};
+use tokio_serial::SerialPortBuilderExt;
+use tokio_util::codec::{BytesCodec, Decoder};
+
+mod arguments;
+
+#[tokio::main]
+async fn main() -> anyhow::Result<()> {
+    tracing_subscriber::fmt::init();
+
+    let args = Arguments::parse();
+
+    match args.command {
+        Command::ListAvailablePorts => {
+            let ports = tokio_serial::available_ports()?;
+            let info = serde_yaml::to_string(&ports)?;
+            println!("{info}");
+        }
+        Command::Connect { tty } => {
+            let mut port = tokio_serial::new(&tty, 115_200).open_native_async()?;
+
+            println!("Connection established on '{tty}'.");
+
+            #[cfg(unix)]
+            port.set_exclusive(false)
+                .context("Unable to set serial port exclusive to false")?;
+
+            let port = BytesCodec::new().framed(port);
+            let (mut writer, mut reader) = port.split::<Bytes>();
+
+            let (tx, mut rx) = mpsc::channel::<HostToSensor>(32);
+
+            let input_handler = spawn_blocking(move || forward_stdin_to_channel(tx));
+
+            loop {
+                select! {
+                    Some(msg) = rx.recv() => {
+                        let bytes = match postcard::to_vec::<_, 64>(&msg).map(|s| s.to_vec()) {
+                            Ok(bytes) => bytes,
+                            Err(err) => {
+                                println!("Found invalid bytes, skipping sending: {err}");
+                                continue;
+                            }
+                        };
+                        let Ok(()) = writer.send(bytes.into()).await else {
+                            break;
+                        };
+                    }
+                    Some(Ok(msg)) = reader.next() => {
+                        let item = match postcard::from_bytes::<SensorToHost>(&msg) {
+                            Ok(item) => item,
+                            Err(err) => {
+                                println!("Found invalid bytes, skipping processing: {err}");
+                                continue;
+                            }
+                        };
+                        if let Err(e) = process_item(item) {
+                            println!("Error while processing item: {e:?}");
+                            continue;
+                        }
+                    }
+                    else => break,
+                }
+            }
+            try_join!(input_handler)?.0?;
+        }
+    }
+
+    Ok(())
+}
+
+fn process_item(item: SensorToHost) -> anyhow::Result<()> {
+    match item {
+        SensorToHost::Id { name, version } => {
+            println!(
+                "Device name: {} (version: {version})",
+                from_utf8(&name).unwrap()
+            );
+        }
+        SensorToHost::Pong => {
+            println!("Received Pong");
+        }
+        SensorToHost::Config(config) => {
+            println!("Got config: {config:#?}");
+        }
+        SensorToHost::ConfigOk => {
+            println!("Received config OK");
+        }
+    }
+    Ok(())
+}
+
+fn forward_stdin_to_channel(tx: mpsc::Sender<HostToSensor>) -> anyhow::Result<()> {
+    let mut buffer = String::new();
+    let stdin = std::io::stdin();
+
+    loop {
+        buffer.clear();
+        let n = match stdin.read_line(&mut buffer) {
+            Ok(0) => {
+                println!("Stdin closed.");
+                break Ok(());
+            }
+            Ok(n) => n,
+            Err(err) => {
+                println!("Failed to read line: {err:?}");
+                break Ok(());
+            }
+        };
+        let Ok(message) = serde_yaml::from_str::<HostToSensor>(&buffer[..n]) else {
+            println!(
+                "Invalid input: {:?}. Try Ping, WhoAreYou, Trigger, or GetConfig",
+                &buffer[..n]
+            );
+            continue;
+        };
+        if let Err(SendError(msg)) = tx.blocking_send(message) {
+            println!("Failed to send {msg:?}, exiting");
+            break Ok(());
+        }
+    }
+}
